5f255d65d4ce57a04f278a8e01738a0dd55ff573
Matt110110 <matruprasadchand@gmail.com>
2018-10-01 22:31:04 +0530

    Complete.

--- AlbenianVirus/MainWindow.xaml.cs
+++ AlbenianVirus/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+/**
+ * I made this app to learn more about WPF and how they work.
+ * So far, they appear to be better than Windows forms and I will explore more by making more such useless apps.
+ * 
+ * This app is utterly useless and was made for fun.
+ * I do not intend to offend anyone from the country of Albania. That is not my intention and hope people take it with humor.
+ * But still, I am sorry if I offended you.
+ */
 namespace AlbenianVirus
 {
     /// <summary>
@@ -27,12 +37,17 @@ namespace AlbenianVirus
 
         private void noButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("You clicked Hell no?");
+            // Closes the WPF form.
+            Close();
         }
 
         private void yesButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("You clicked yes?");
+            /*
+             * Opens the C:\Program Files folder in a new window.
+             * This will work on all windows computers because in both x86 and x64 C:\Program Files is present.
+             */
+            Process.Start(@"C:\Program Files");
         }
     }
 }
